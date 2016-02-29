7c0919b321386658aae8b4200d3a88c7082e3092
Eleanor <eleanor.weigert@byhook.com>
2016-02-29 15:56:30 -0800

    Basic usage of canvas

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 'use strict'
 
-var canvas = document.getElementById('canvas');
+var body = $('body');
 
-
-//can check for support by doing..
-if(canvas.getContext) {
-  var context = canvas.getContext('2d'); //specifies that we want a 2d context for rendering
-} else {
-  //stuff for when the canvas isn't supported
+window.onload = function() {
+  draw();
+  console.log('i drew!');
 }
 
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if(canvas.getContext) {
+    var context = canvas.getContext('2d');   
+
+    context.fillStyle = "rgb(200,0,0)";
+    context.fillRect (10, 10, 55, 50);
+
+    context.fillStyle = "rgba(0, 0, 200, 0.5)";
+    context.fillRect (30, 30, 55, 50);
+  } else {
+    console.log('doesnt look like canvas is supported');
+  }
+}
 
 
 
